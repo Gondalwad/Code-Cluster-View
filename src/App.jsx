44285bf2d86f86ee951ad14c49995d0c1ba55965
gondalwad <sudarshangondalwad@gmail.com>
2025-09-06 01:24:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from "flowbite-react";
+
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Test Flowbite</Button>
+    </div>
+  );
+}
